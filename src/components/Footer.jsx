495c02dd5c8d1cf5d479b7720aabf4775171d637
krishna <krishnakrrish66@gmail.com>
2023-08-14 20:38:01 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyMaster</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://github.com/krishnamidathana" target={"blank"}>
+            Github
+          </a>
+          <a href="https://instagram.com/krishnamohan9661" target={"blank"}>
+            Instagram
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
