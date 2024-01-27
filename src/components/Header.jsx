9e4be5ce4df565e3e755d4ef91645c449446884c
KRISHNA MOHAN <134590019+krishnamidathana@users.noreply.github.com>
2024-01-27 12:53:14 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     <nav>
       <h1>TechyMaster</h1>
       <main>
-        <HashLink to={"/#home"}>Home</HashLink>
+       
         <Link to={"/contact"}>Contact</Link>
         <HashLink to={"/#about"}>About</HashLink>
         <HashLink to={"/#brands"}>Brands</HashLink>
